cce18e1fb1cac04b9b31f4f420cecefab5d8e46c
vnepveu <victor@flaneer.com>
2021-09-27 22:48:26 +0200

    feat: fetch prices from pools

---
+++ frontend/src/pool_interfaces.ts
@@ -0,0 +1,21 @@
+import {ethers} from "ethers";
+
+export interface PoolImmutables {
+    factory: string;
+    token0: string;
+    token1: string;
+    fee: number;
+    tickSpacing: number;
+    maxLiquidityPerTick: ethers.BigNumber;
+}
+
+export interface PoolState {
+    liquidity: ethers.BigNumber;
+    sqrtPriceX96: ethers.BigNumber;
+    tick: number;
+    observationIndex: number;
+    observationCardinality: number;
+    observationCardinalityNext: number;
+    feeProtocol: number;
+    unlocked: boolean;
+}
